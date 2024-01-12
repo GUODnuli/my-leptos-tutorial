5f392f80e20f6ffa5f5367e9e8b8f13c44121e2a
zhuangxiaosen <xz.ganbaruibro@gmail.com>
2024-01-12 19:01:08 +0800

    24.1.12

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,14 @@ fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
 
     view! {
-        <button on:click=move|_| { set_count.update(|n| *n += 1); }>
+        <button 
+            on:click=move|_| { set_count.update(|n| *n += 1); }
+            class:red=move || count()%2 == 1
+            style="position:absolute"
+            style:left=move || format!("{}px", x() + 100)
+            style:backgroud-color=move || format!("rgb({}, {}, 100)", x(), 100)
+            style
+        >
             "Click me: "
             {count}
         </button>
