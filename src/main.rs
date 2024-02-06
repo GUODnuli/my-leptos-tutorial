73dfbb73a808f4d411b0c7762f59bcd9b9cf4b3e
zhuangxiaosen <624115642@qq.com>
2024-02-06 17:09:00 +0800

    24.2.6:Error Handling, completed.

--- src/main.rs
+++ src/main.rs
@@ -153,6 +153,10 @@ fn App() -> impl IntoView {
         <br/>
 
         <ReturndifferentHTML value=option_value set_value=set_option_value/>
+
+        <br/>
+
+        <NumericInput/>
     }
 }
 
@@ -435,5 +439,40 @@ fn ReturndifferentHTML(value: ReadSignal<i32>, set_value: WriteSignal<i32>) -> i
                 }
             }
         </main>
+    }
+}
+
+#[component]
+fn NumericInput() -> impl IntoView {
+    let (value, set_value) = create_signal(Ok(0));
+
+    // when input changes, try to parse a number from the input
+    let on_input = move |ev| set_value(event_target_value(&ev).parse::<i32>());
+
+    view! {
+        <h1>"Error Handling"</h1>
+        <label>
+            "Tyep a number (or something that's not a number!)"
+            <input type="number" on:input=on_input/>
+            <ErrorBoundary
+                // the fallback receives a signal containing current errors
+                fallback=|errors| view! {
+                    <div vlass="error">
+                        <p>"Not a number! Errors:"</p>
+                        // we can rander a list of errors as strings, if we'd like
+                        <ul>
+                            {
+                                move || errors.get()
+                                    .into_iter()
+                                    .map(|(_, e)| view! { <li>{e.to_string()}</li> })
+                                    .collect_view()
+                            }
+                        </ul>
+                    </div>
+                }
+            >
+                <p>"You entered " <strong>{value}</strong></p>
+            </ErrorBoundary>    
+        </label>
     }
 }
